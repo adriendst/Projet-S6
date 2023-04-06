7870ec90718a8cd6ece2a29019977cae81b8e4b9
Yann Hippert <yann.hippert@hotmail.com>
2023-04-06 13:35:14 +0200

    Removed back to top

--- frontend/src/SearchPage/SearchPage.tsx
+++ frontend/src/SearchPage/SearchPage.tsx
@@ -1,107 +1,99 @@
-import React, {useEffect} from 'react';
-import DisplayButton from "../Filter/DisplayButton";
-import TableData from "./TableData";
-import {useDispatch, useSelector} from "react-redux";
-import {State} from "../store";
-import TileData from "./TileData/TileData";
-import Filter from "../Filter/Filter";
-import Layout from "../Layout/Layout";
-import './SearchPage.css'
-import {FaArrowAltCircleUp} from 'react-icons/fa'
-import {changeUrl, loadGames, loadGenres, loadCategories, userConnection} from "../Slice/Slice";
-import axios from "axios";
-
+import React, { useEffect } from 'react';
+import DisplayButton from '../Filter/DisplayButton';
+import TableData from './TableData';
+import { useDispatch, useSelector } from 'react-redux';
+import { State } from '../store';
+import TileData from './TileData/TileData';
+import Filter from '../Filter/Filter';
+import Layout from '../Layout/Layout';
+import './SearchPage.css';
+import { FaArrowAltCircleUp } from 'react-icons/fa';
+import { changeUrl, loadGames, loadGenres, loadCategories, userConnection } from '../Slice/Slice';
+import axios from 'axios';
 
 const SearchPage = () => {
     const displayTable = useSelector((state: State) => state.steam.displayType);
 
-    const dispatch = useDispatch()
+    const dispatch = useDispatch();
 
     const handleClick = () => {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    }
-
+        window.scrollTo({ top: 0, behavior: 'smooth' });
+    };
 
     const games = useSelector((state: State) => state.steam.game);
     useEffect(() => {
-        if(games) {
+        if (games) {
             if (games.length === 0) {
-                fetch("http://localhost:9090/v1/genre/all")
-                    .then(response => response.json())
-                    .then(response => {
+                fetch('http://localhost:9090/v1/genre/all')
+                    .then((response) => response.json())
+                    .then((response) => {
                         dispatch(loadGenres(response));
                     })
-                    .catch(error => alert("Erreur : " + error));
-                fetch("http://localhost:9090/v1/category/all")
-                    .then(response => response.json())
-                    .then(response => {
+                    .catch((error) => alert('Erreur : ' + error));
+                fetch('http://localhost:9090/v1/category/all')
+                    .then((response) => response.json())
+                    .then((response) => {
                         dispatch(loadCategories(response));
                     })
-                    .catch(error => alert("Erreur : " + error));
+                    .catch((error) => alert('Erreur : ' + error));
             }
         }
-
-
     }, []);
-
 
     const filters = useSelector((state: State) => state.steam.filter);
     useEffect(() => {
-        let test = 'http://localhost:9090/v1/game/filter/1?'
-        const filtersRecord = filters as Record<string, any>
+        let test = 'http://localhost:9090/v1/game/filter/1?';
+        const filtersRecord = filters as Record<string, any>;
         // console.log(filtersRecord)
         for (let filtersKey in filters) {
-            if (typeof filtersRecord[filtersKey] !== "boolean") {
+            if (typeof filtersRecord[filtersKey] !== 'boolean') {
                 if (filtersRecord[filtersKey] !== undefined && filtersRecord[filtersKey].length !== 0 && filtersRecord[filtersKey] !== null) {
                     if (typeof filtersRecord[filtersKey] === 'object') {
                         if (filtersKey === 'release_date') {
                             if (filtersRecord[filtersKey][0]) {
                                 if (filtersRecord[filtersKey][1]) {
-                                    test = test + `&start_date=${filtersRecord[filtersKey][0]}&end_date=${filtersRecord[filtersKey][1]}`
+                                    test = test + `&start_date=${filtersRecord[filtersKey][0]}&end_date=${filtersRecord[filtersKey][1]}`;
                                 } else {
-                                    test = test + `&start_date=${filtersRecord[filtersKey][0]}`
+                                    test = test + `&start_date=${filtersRecord[filtersKey][0]}`;
                                 }
                             }
                         } else {
                             for (let i = 0; i < filtersRecord[filtersKey].length; i++) {
-                                test = test + `&${filtersKey}=${filtersRecord[filtersKey][i]}`
+                                test = test + `&${filtersKey}=${filtersRecord[filtersKey][i]}`;
                             }
-                        // &start_date=1999-01-01&end_date=2022-12-31
+                            // &start_date=1999-01-01&end_date=2022-12-31
                         }
                     } else {
-                        if(!isNaN(filtersRecord[filtersKey]))
-                            console.log('coucouc')
-                        test = test + `&${filtersKey}=${filtersRecord[filtersKey]}`
+                        if (!isNaN(filtersRecord[filtersKey])) console.log('coucouc');
+                        test = test + `&${filtersKey}=${filtersRecord[filtersKey]}`;
                     }
                 }
-            }
-            else{
-                console.log(filtersKey)
-                if(filtersKey === 'user_only'){
-                    if(filtersRecord[filtersKey] === true){
-                        test = test + `&${filtersKey}=${filtersRecord[filtersKey]}`
+            } else {
+                console.log(filtersKey);
+                if (filtersKey === 'user_only') {
+                    if (filtersRecord[filtersKey] === true) {
+                        test = test + `&${filtersKey}=${filtersRecord[filtersKey]}`;
                     }
                 }
             }
         }
-        dispatch(changeUrl(test))
+        dispatch(changeUrl(test));
 
-        console.log(test)
+        console.log(test);
 
-        axios.get(test)
-            .then(response => response.data)
-            .then(response => {
+        axios
+            .get(test)
+            .then((response) => response.data)
+            .then((response) => {
                 dispatch(loadGames([response.results, 2]));
             })
-            .catch(error => alert("Erreur : " + error));
-
+            .catch((error) => alert('Erreur : ' + error));
     }, [filters]);
-
 
     return (
         <div>
             <div className={'searchLayout'}>
-                <Layout/>
+                <Layout />
                 <div className={'filterLayout'}>
                     <Filter></Filter>
                     <div className={'displayButton'}>
@@ -110,8 +102,8 @@ const SearchPage = () => {
                 </div>
             </div>
             <div className={'data'}>
-                {!displayTable ? <TileData/> : <TableData/>}
-                <FaArrowAltCircleUp size={40} className={'arrow'} onClick={handleClick}/>
+                {!displayTable ? <TileData /> : <TableData />}
+                {/* <FaArrowAltCircleUp size={40} className={'arrow'} onClick={handleClick}/> */}
             </div>
         </div>
     );
